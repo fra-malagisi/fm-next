06b08ba81adbe10385206e42d68a36fa2e29b341
francesco.malagisi <francesco.malagisi@accenture.com>
2022-04-07 15:54:37 +0200

    feat: add input component

---
+++ components/input/input.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx';
+import { forwardRef, InputHTMLAttributes } from 'react';
+import { FieldError } from 'react-hook-form';
+
+import styles from './input.module.css';
+
+export type InputProps = {
+  name: string;
+  label: string;
+  autoComplete?: string;
+  id?: string;
+  error?: FieldError;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+const Input = forwardRef<HTMLInputElement, InputProps>(({ name, id, label, error, ...props }, ref) => {
+  const inputClasses = clsx(styles.input, {
+    [styles['input--success'] as string]: !error,
+    [styles['input-error'] as string]: error,
+  });
+
+  const labelClasses = clsx(styles['input-label'], {
+    [styles['input-label--success'] as string]: !error,
+    [styles['input-label--error'] as string]: error,
+  });
+
+  return (
+    <div>
+      <label htmlFor={name} className={labelClasses}>
+        {label}
+      </label>
+      <input ref={ref} type='text' name={name} id={id ?? name} className={inputClasses} {...props} />
+      {error && (
+        <p data-testid={`${name}-error`} className={styles['input-error__message']}>
+          {error.message}
+        </p>
+      )}
+    </div>
+  );
+});
+
+Input.displayName = 'InputComponent';
+
+export default Input;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import { Input } from 'components/input';
 import { FC } from 'react';
 
 const Home: FC = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Input name='prova' label='prova2' />
+    </>
+  );
 };
 
 export default Home;
