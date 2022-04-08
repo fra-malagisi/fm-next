c2bf729b1d953637614a0bd08e1943b95c74955d
francesco.malagisi <francesco.malagisi@accenture.com>
2022-04-08 18:13:54 +0200

    feat: add firt query and api route

---
+++ pages/api/movies/index.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import dbConnect from '../../../lib/dbConnect';
+import Movie from '../../../models/movie';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case 'GET':
+      try {
+        const movies = await Movie.find({}); /* find all the data in our database */
+        res.status(200).json({ success: true, data: movies });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case 'POST':
+      try {
+        const movie = await Movie.create(req.body); /* create a new model in the database */
+        res.status(201).json({ success: true, data: movie });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
-import { Input } from 'components/input';
+import { NextPage } from 'next';
 import { FC } from 'react';
 
-const Home: FC = () => {
+import dbConnect from '../lib/dbConnect';
+import MovieSchema, { Movie } from '../models/movie';
+
+const Home: NextPage = () => {
   return (
     <>
       <h1>Home</h1>
-      <Input name='prova' label='prova2' />
     </>
   );
 };
 
 export default Home;
+
+/* Retrieves pet(s) data from mongodb database */
+export async function getServerSideProps() {
+  await dbConnect();
+
+  /* find all the data in our database */
+  const result: Movie[] = await MovieSchema.find({}).limit(10);
+  return { props: { movies: JSON.parse(JSON.stringify(result)) } };
+}
