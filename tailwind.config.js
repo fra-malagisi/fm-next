1b9ce0cbb78e114f11264ae1c05710d76103cb0a
francesco.malagisi <francesco.malagisi@accenture.com>
2022-04-12 18:35:27 +0200

    feat: tailwind theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: {},
+      textColor: {
+        skin: {
+          base: 'var(--color-text-base)',
+          muted: 'var(--color-text-muted)',
+          inverted: 'var(--color-text-inverted)',
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: withOpacity('--color-fill'),
+          'button-accent': 'var(--color-button-accent)',
+          'button-accent-hover': 'var(--color-button-accent-hover)',
+          'button-muted': 'var(--color-button-muted)',
+        },
+      },
+      gradientColorStops: {
+        skin: {
+          hue: 'var(--color-fill)',
+        },
+      },
     },
   },
   plugins: [
