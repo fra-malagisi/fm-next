1ce34b2cf1400246a5055fababdb32e539fe4aed
francesco.malagisi <francesco.malagisi@accenture.com>
2022-04-19 22:00:34 +0200

    fix: wrap content in layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import { Layout } from 'components/layout';
 import { AppProps } from 'next/app';
 import '../styles/global.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+);
 
 export default MyApp;
