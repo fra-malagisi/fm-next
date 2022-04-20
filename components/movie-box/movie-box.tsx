cc7182f97f73965fb2f35fe57bf027f9f4fc7bd1
francesco.malagisi <francesco.malagisi@accenture.com>
2022-04-20 09:17:56 +0200

    feat: add movie box

---
+++ components/movie-box/movie-box.tsx
@@ -0,0 +1,24 @@
+import { Movie } from 'models/movie';
+import Image from 'next/image';
+import { FC } from 'react';
+
+import styles from './movie-box.module.css';
+
+export type MovieBoxProps = {
+  movie: Movie;
+};
+
+const MovieBox: FC<MovieBoxProps> = ({ movie }) => {
+  return (
+    <article className={styles['movie-box']}>
+      <div className={styles['movie-box__image']}>
+        <Image src={movie.imgSrc} alt={`${movie.title} image`} width={100} height={150} layout='responsive' />
+      </div>
+      <div className={styles['movie-box__content']}>
+        <p>{movie.overview}</p>
+      </div>
+    </article>
+  );
+};
+
+export default MovieBox;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
-import { Header } from 'components';
+import MovieBox from 'components/movie-box';
 import { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { movieConverter } from 'utils';
 
 import dbConnect from '../lib/dbConnect';
-import MovieSchema, { Movie } from '../models/movie';
+import MovieSchema, { Movie, MovieBe } from '../models/movie';
 
-const Home: NextPage = () => {
+const Home: NextPage<{ movies: Movie[] }> = ({ movies }) => {
   return (
     <>
-      <Header title='fm-next' />
-      <h1>Home</h1>
+      <div className='grid grid-cols-3 gap-4'>
+        {movies && movies.map(movie => <MovieBox key={movie.id} movie={movie} />)}
+      </div>
     </>
   );
 };
@@ -21,6 +23,6 @@ export async function getServerSideProps() {
   await dbConnect();
 
   /* find all the data in our database */
-  const result: Movie[] = await MovieSchema.find({}).limit(10);
-  return { props: { movies: JSON.parse(JSON.stringify(result)) } };
+  const result: MovieBe[] = await MovieSchema.find({}).limit(10);
+  return { props: { movies: JSON.parse(JSON.stringify(result.map(movieBe => movieConverter(movieBe)))) } };
 }
