39dd42f363544f25151466f534995f24296cc4b6
francesco.malagisi <francesco.malagisi@accenture.com>
2022-04-27 09:55:56 +0200

    fix: add total property to getmovie response

--- hooks/movie.ts
+++ hooks/movie.ts
@@ -5,17 +5,23 @@ import useSWR from 'swr';
 
 import fetcher from './utils/fetcher';
 
+export type MoviesWithPagination = {
+  movies: Movie[];
+  total: number;
+};
+
 const useMovie = (
   paginationQuery: PaginationQuery
 ): {
   setPagination: Dispatch<SetStateAction<PaginationQuery>>;
   movies: Movie[] | undefined;
+  total: number | undefined;
   error: Error | undefined;
   isLoading: boolean;
 } => {
   const host = `${process.env.NEXT_PUBLIC_API_HOST}`;
   const [pagination, setPagination] = useState<PaginationQuery>(paginationQuery);
-  const { data, error } = useSWR<Movie[], Error>(
+  const { data, error } = useSWR<MoviesWithPagination, Error>(
     `http://localhost:3001/api/movies/pagination?limit=${pagination.limit}&offset=${pagination.offset}`,
     fetcher,
     {}
@@ -23,7 +29,8 @@ const useMovie = (
 
   return {
     setPagination,
-    movies: data,
+    movies: data && data.movies,
+    total: data && data.total,
     error,
     isLoading: !data && !error,
   };
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,17 @@ import { movieConverter } from 'utils';
 import dbConnect from '../lib/dbConnect';
 import MovieSchema, { Movie, MovieBe } from '../models/movie';
 
-const Home: NextPage<{ movies: Movie[] }> = () => {
-  const { movies, setPagination } = useMovie({ limit: 1, offset: 0 });
-  setTimeout(() => setPagination({ limit: 1, offset: 1 }), 2000);
+const Home: NextPage<{
+  /*movies: Movie[]*/
+}> = () => {
+  const { movies, total, setPagination } = useMovie({ limit: 1, offset: 0 });
   return (
     <>
       <div className='grid grid-cols-3 gap-4'>
+        {total}
         {movies && movies.map(movie => <MovieBox key={movie.id} movie={movie} />)}
+        <button onClick={() => setPagination(prev => ({ limit: 1, offset: prev.offset + 1 }))}>+</button>
+        <button onClick={() => setPagination(prev => ({ limit: 1, offset: prev.offset - 1 }))}>-</button>
       </div>
     </>
   );
@@ -22,10 +26,10 @@ const Home: NextPage<{ movies: Movie[] }> = () => {
 export default Home;
 
 /* Retrieves pet(s) data from mongodb database */
-export async function getServerSideProps() {
+/* export async function getServerSideProps() {
   await dbConnect();
 
-  /* find all the data in our database */
+  /* find all the data in our database 
   const result: MovieBe[] = await MovieSchema.find({}).limit(10);
   return { props: { movies: JSON.parse(JSON.stringify(result.map(movieBe => movieConverter(movieBe)))) } };
-}
+} */
