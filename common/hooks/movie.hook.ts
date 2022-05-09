c38fa48b77c378f2c90285173e5be93bff5ff4a4
francesco.malagisi <francesco.malagisi@accenture.com>
2022-05-09 18:57:18 +0200

    feat: add movie filter in search api

--- common/hooks/movie.hook.ts
+++ common/hooks/movie.hook.ts
@@ -11,8 +11,7 @@ export type MoviesWithPagination = {
 };
 
 const useMovie = (
-  paginationQuery: PaginationQuery,
-  fallbackData?: MoviesWithPagination
+  paginationQuery: PaginationQuery
 ): {
   setPagination: Dispatch<SetStateAction<PaginationQuery>>;
   movies: Movie[] | undefined;
@@ -22,23 +21,17 @@ const useMovie = (
 } => {
   const host = `${process.env.NEXT_PUBLIC_API_HOST}`;
   const [pagination, setPagination] = useState<PaginationQuery>(paginationQuery);
-  const [total, setTotal] = useState<number>(0);
   const { data, error } = useSWR<MoviesWithPagination, Error>(
-    `http://localhost:3001/api/movies/pagination?limit=${pagination.limit}&offset=${pagination.offset}`,
-    fetcher,
-    { fallbackData }
+    `http://localhost:3001/api/movies/pagination?limit=${pagination.limit}&offset=${pagination.offset}${
+      pagination.title ? `&title=${pagination.title}` : ''
+    }`,
+    fetcher
   );
-
-  useEffect(() => {
-    if (data && data?.total > 0 && data?.total !== total) {
-      setTotal(data?.total || 0);
-    }
-  }, [data?.total, total]);
 
   return {
     setPagination,
     movies: data && data.movies,
-    total,
+    total: data?.total,
     error,
     isLoading: !data && !error,
   };
--- models/common.ts
+++ models/common.ts
@@ -1,6 +1,7 @@
 interface PaginationQuery {
   limit: number;
   offset: number;
+  title?: string;
 }
 
 export default PaginationQuery;
